3a2ac9edf4b0a49fa6ab86ce2dfb2c61e59de3b1
Andres Carvajal <jc1993box@hotmail.com>
2020-04-25 00:39:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 def reverse_each_word(string)
-  array = string.split(" ")
-  new_arr string.reverse.each do |word|
+  string.each.split {|word|word.reverse}.join (" ")
 end
 
 
-def reverse_string(str)
-  arr = str.split(" ")
-  new_arr = arr.collect{|a| a.reverse}
-  new_arr.join(" ")
-end
