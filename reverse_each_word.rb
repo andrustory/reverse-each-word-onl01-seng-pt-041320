c96811dfed82b2769b5a54c67e2b09114a306d62
Andres Carvajal <jc1993box@hotmail.com>
2020-04-25 01:00:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-# def reverse_each_word(sentence)
-#   sentence.split.collect{|word|word.reverse}.join (" ")
-# end
+def reverse_each_word(sentence)
+  sentence.split.collect{|word|word.reverse}.join (" ")
+end
 
 
 
@@ -16,10 +16,10 @@
 # end
 
 
-def reverse_each_word(sentence)
-  new_array = []
-  (sentence.split).each do |word|
-    new_array << word.reverse 
-  end
-  new_array.join(" ")
-end 
+# def reverse_each_word(sentence)
+#   new_array = []
+#   (sentence.split).each do |word|
+#     new_array << word.reverse 
+#   end
+#   new_array.join(" ")
+# end 
