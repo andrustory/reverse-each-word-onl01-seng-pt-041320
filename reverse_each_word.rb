25800ca21b20c60511d6dbf502c22d460cac42ed
Andres Carvajal <jc1993box@hotmail.com>
2020-04-25 00:24:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,11 @@
 def reverse_each_word(string)
-  string.reverse.each do |word|
+  array = string.split(" ")
+  new_arr string.reverse.each do |word|
+end
+
+
+def reverse_string(str)
+  arr = str.split(" ")
+  new_arr = arr.collect{|a| a.reverse}
+  new_arr.join(" ")
 end
